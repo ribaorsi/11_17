613c1c361eb9693c850bec6d1141cfa3207fac81
ribaorsi <ribaorsi@gmail.com>
2021-11-18 12:37:49 +0100

    fixed

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const generateArray = function (amount){
+const generateArray = function(amount){
 
     for (let i = 0; i < amount.length; i++) {
         let returnArray = [];
+        /*
+        let amountConverted = parseInt(amount);
+        amount = amountConverted;
+        */
+        amount = parseInt(amount);
+
+        //Number.isNaN(amount)!== true
+        if (!Number.isNaN(amount)){
+
+        }
+        else {
+            returnArray.push("error");
+        }
+
         returnArray.push(`${i}`);
         //console.log(returnArray);
         
     }
-    console.log(returnArray);
+    //console.log(returnArray);
+    return returnArray;
 }
 
 
 function loadEvent() {
     console.log("The page has loaded");
 
+    /*console.log(generateArray("kiskutya"));
+    console.log(generateArray("1 kiskutya"));
     console.log(generateArray(100));
+    console.log(generateArray("100"));
+    console.log(generateArray([100]));
+    */
+   const root = document.getElementById("root");
+   const list = generateArray("6");
+   console.log(list);
+
+   if (list[0] !== "error") {
+       for (const item of list) {
+         root.insertAdjacentHTML("beforeend", `<div>${item}</div`)  
+       }
+   }
 }
 
 window.addEventListener("load", loadEvent);
